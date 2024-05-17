bd3aaa20d7f1ce9a865c57a5dae6b83e47adcd73
JhoonHe <jjhonherrera113@gmail.com>
2024-05-17 16:30:03 -0500

    commit #3

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
-const start = document.getElementById('start')
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const start = document.getElementById('start');
 
 window.addEventListener("DOMContentLoaded", pressButton);
 
-function pressButton(){
-    rock.addEventListener("click", (event) => pressedButton(event, "rock"))
-    paper.addEventListener("click", (event) => pressedButton(event, "paper"))
-    scissors.addEventListener("click", (event) => pressedButton(event, "scissors"))
+function pressButton() {
+    rock.addEventListener("click", (event) => pressedButton(event, "rock"));
+    paper.addEventListener("click", (event) => pressedButton(event, "paper"));
+    scissors.addEventListener("click", (event) => pressedButton(event, "scissors"));
 }
 
-function pressedrock (){
-    let answer = "rock"
-    console.log(answer)
-    return answer
-}
+function pressedButton(event, buttonType) {
 
-function pressedpaper (){
-    let answer = "paper"
-    console.log(answer)
-    return answer
-}
-
-function pressedButton (event, buttonType){
     let answer = buttonType;
-    console.log(answer) 
-    return answer
+    console.log(answer);
+
+
+    
+    const opciones = ["rock", "paper", "scissors"];
+    const bot = opciones[Math.floor(Math.random() * opciones.length)];
+    console.log("bot election:", bot);
+    winner(answer, bot);
 }
 
-const opciones = ["rock", "paper", "scissors"];
-
-const bot = opciones[Math.floor(Math.random() * opciones.length)];
-
-winner(answer,bot) {
-  
-    if (answer == bot) {
-        console.log('Draw')
-    } else if((answer == "paper" && bot == "rock") || (answer == "scissors" && bot == "paper")|| (answer == "rock" && bot == "scissors")
-     ) {
-        console.log('You won')
+function winner(answer, bot) {
+    if (answer === bot) {
+        console.log('Draw');
+    } else if ((answer === "paper" && bot === "rock") || 
+               (answer === "scissors" && bot === "paper") || 
+               (answer === "rock" && bot === "scissors")) {
+        console.log('You won');
     } else {
-       console.log('You lost')
+       console.log('You lost');
     }
 }
